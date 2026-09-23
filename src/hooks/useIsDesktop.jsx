09df6dea5a6848b2b3e0a06797253ef04d3fcf11
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-09-23 20:38:25 +0300

    fix hydration error

--- src/hooks/useIsDesktop.jsx
+++ src/hooks/useIsDesktop.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 export default function useIsDesktop(breakpoint = 1024) {
-  const [isDesktop, setIsDesktop] = React.useState(
-    () => typeof window !== "undefined" && window.innerWidth > breakpoint
-  );
+  const [isDesktop, setIsDesktop] = React.useState(false);
 
   React.useEffect(() => {
     const handleResize = () => setIsDesktop(window.innerWidth > breakpoint);
