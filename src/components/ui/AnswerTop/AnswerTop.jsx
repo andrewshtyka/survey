51b8c600ce4e45e287ebf9f22bd28853bbc8b573
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-09-25 19:53:28 +0300

    fix heading

--- src/components/ui/AnswerTop/AnswerTop.jsx
+++ src/components/ui/AnswerTop/AnswerTop.jsx
@@ -25,13 +25,24 @@ import React from "react";
 // #endregion ===========================
 
 export default function AnswerTop({ data }) {
+    const amount = data.question.length;
+
     return <>
         <p className={`f_mono_xl ${css.number_container}`}>{data.id}</p>
         
         <h2 className={`f_h5_helvetica ${css.h2}`}>
-            {data.question.map((el, i) => (
-                <span key={i}>{el}</span>
-            ))}
+            {data.question.map((el, i) => {
+                if (amount > 1) {
+                    const appliedStyle = i === 1 ?
+                    {} : {
+                            display: "inline-flex",
+                            marginBlockEnd: "var(--space-10)"
+                        };
+                    return  <span key={i} style={appliedStyle}>{el}</span>
+                } else if (amount === 1) {
+                    return  <span key={i} >{el}</span>
+                }
+            })}
         </h2>
     </>;
 }
